76c2cdf01500da80e11360439c67f3a5e75519c1
YadiBelly <Yadisabell96@gmail.com>
2022-10-13 22:58:04 +0000

    updated

--- src/app.js
+++ src/app.js
@@ -1,4 +1,3 @@
-/* eslint-disable */
 import "bootstrap";
 import "./style.css";
 
@@ -6,6 +5,34 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Card Generator");
+  var suits = ["&spades;", "&hearts;", "&clubs;", "&diams;"];
+  var values = [
+    "A",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "Q",
+    "K"
+  ];
+
+  let randomValues = Math.floor(Math.random() * 12);
+  let randomSuits = Math.floor(Math.random() * 4);
+
+  document.querySelector(".top-icon").innerHTML = suits[randomSuits];
+  document.querySelector(".bottom-icon").innerHTML = suits[randomSuits];
+  document.querySelector(".number").innerHTML = values[randomValues];
+  console.log("Hello Rigo from the console!");
+
+  if (randomSuits == 1 || randomSuits == 3) {
+    document.querySelector(".top-icon").style.color = "red";
+    document.querySelector(".bottom-icon").style.color = "red";
+    document.querySelector(".number").style.color = "red";
+  }
 };
